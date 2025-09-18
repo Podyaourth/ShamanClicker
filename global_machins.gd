329b9a32c897821e702a930867921da222a199dd
Podyaourth <podyaourth@gmail.com>
2025-09-18 03:47:16 +0200

    test commit

--- global_machins.gd
+++ global_machins.gd
@@ -1,7 +1,7 @@
 extends Node
 
 var RemousToolTip : String = "Remous : fais des trucs forts.\nCoût en Mana : X"
-var AffluxToolTip : String = "Afflux : fais des trucs plus forts.\nCoût en Mana : X"
+var AffluxToolTip : String = "Afflux : fais des trucs plus forts.\nCoût en Mana : LA BITE"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
